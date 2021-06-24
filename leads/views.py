4daf87c03bd8eda84d75472ba0453948f3ad50b6
Matt Stephens <matt@bestlifeseniorliving.com>
2021-06-24 15:58:50 -0400

    First commit

---
+++ leads/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home_page(request):
+    # return HttpResponse("Hello world")
+    return render(request, "leads/home_page.html")
+
