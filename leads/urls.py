e7c8b9d065f98d7bf5f72c126a7555d6f8477214
Matt Stephens <matt@bestlifeseniorliving.com>
2021-06-25 14:47:54 -0400

    leads app and forms

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from .views import home_page
+from .views import lead_list, lead_detail, lead_create
 
 app_name = "leads"
 
 urlpatterns = [
-    path('', home_page)
+    path('', lead_list),
+    path('<int:pk>/', lead_detail),
+    path('create/', lead_create),
 ]
