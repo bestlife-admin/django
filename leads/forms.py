d5ea4abdc79a1098e9a99771914c73002098f837
Matt Stephens <matt@bestlifeseniorliving.com>
2021-07-09 01:23:07 -0400

    Up to the last update prior to Postgres/createdb command

--- leads/forms.py
+++ leads/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import Lead
-
+from .models import Lead, Agent, Category
 
 User = get_user_model()
 
@@ -16,6 +15,9 @@ class LeadModelForm(forms.ModelForm):
             'last_name',
             'age',
             'agent',
+            'description',
+            'phone_number',
+            'email',
         )
 
 
@@ -29,4 +31,39 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username",)
-        field_classes = {'username': UsernameField}	
+        field_classes = {'username': UsernameField}
+
+
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents = Agent.objects.filter(organisation=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields["agent"].queryset = agents
+
+
+class LeadCategoryUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = (
+            'category',
+        )
+
+
+# class CategoryModelForm(forms.ModelForm):
+#     class Meta:
+#         model = Category
+#         fields = (
+#             'name',
+#         )
+
+
+# class FollowUpModelForm(forms.ModelForm):
+#     class Meta:
+#         model = FollowUp
+#         fields = (
+#             'notes',
+#             'file'
+#         )
